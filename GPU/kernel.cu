af6451445e58eb06d128bab4fd8be54addb37474
nighthawkbravo <lucas@aggati.com>
2022-01-27 01:10:56 +0100

    Added Cuda helper function for the password cracker

--- GPU/kernel.cu
+++ GPU/kernel.cu
@@ -1,17 +1,30 @@
 ﻿
 #include "cuda_runtime.h"
+#include "RandomString.h"
 #include "device_launch_parameters.h"
 #include "sha256.h"
 #include <string>
 #include <math.h> 
 #include <iostream>
-
-
 #include <stdio.h>
 
+#define LEN 3
+#define BASE 74
+#define DIVISIONS 5
+#define BOTTOM 48
+#define TOP 122
+
+
+
+
+
+using std::string;
 using std::cout;
+using std::endl;
 
 cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+
+cudaError_t crackWithCuda(int len, int base, int div, int bottom, int top, int* password, int** arr);
 
 int** preprocess(int len, int base, int div, int bottom, int top);
 
@@ -23,20 +36,39 @@ __global__ void addKernel(int *c, const int *a, const int *b)
 
 int main()
 {
-    /*const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };*/
-
-    // Add vectors in parallel.
-    /*cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
+    
+    // Add vectors in parallel.    
+    /*
+    
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "addWithCuda failed!");
         return 1;
     }
 
     printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);*/
+        c[0], c[1], c[2], c[3], c[4]);
+        
+        */
+
+
+    RandomString RS(LEN);
+
+    cout << endl;
+    cout << "Password: " << RS.getPassword() << endl;
+    //cout << "Password Hash: " << RS.getHashPassword() << endl;
+    cout << endl;
+    
+    int* passwordInt = RS.convertToIntArr(RS.getPassword(), LEN);    
+
+    
+    //cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
+    
+    int** ranges = preprocess(LEN, BASE, DIVISIONS, BOTTOM, TOP);
+    crackWithCuda(LEN, BASE, DIVISIONS, BOTTOM, TOP, passwordInt, ranges);
+    
+
+    
+
 
     // cudaDeviceReset must be called before exiting in order for profiling and
     // tracing tools such as Nsight and Visual Profiler to show complete traces.
@@ -46,16 +78,8 @@ int main()
         return 1;
     }*/
 
-    int** a = preprocess(2,74,5,48,122);
-
-
-    delete[] a;
-
-
-
-
-
-
+    delete[] ranges;
+    delete passwordInt;
     return 0;
 }
 
@@ -139,6 +163,16 @@ Error:
     return cudaStatus;
 }
 
+
+/// <summary>
+/// This function...
+/// </summary>
+/// <param name="len">: The length of the password.</param>
+/// <param name="base">: The number of different symbols used in the password.</param>
+/// <param name="div">: The number of subdivisions to take of the password.</param>
+/// <param name="bottom">: The lowest code from the symbols.</param>
+/// <param name="top">: The highest code from the symbols.</param>
+/// <returns></returns>
 int** preprocess(int len, int base, int div, int bottom, int top) {
 
     unsigned long long* base10 = new unsigned long long[2*div];
@@ -196,3 +230,104 @@ int** preprocess(int len, int base, int div, int bottom, int top) {
     //delete[] baseB;
     delete base10;
 }
+
+
+
+cudaError_t crackWithCuda(int len, int base, int div, int bottom, int top, int* password, int** arr)
+{
+    int rangeSize = (2 * div);
+    int* dev_password = 0;
+    int** dev_ranges = 0;
+    
+    cudaError_t cudaStatus;
+
+    // Choose which GPU to run on, change this on a multi-GPU system.
+    cudaStatus = cudaSetDevice(0);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+        goto Error;
+    }
+
+
+    cudaStatus = cudaMalloc((void**)&dev_password, len * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMalloc((void**)&dev_ranges, sizeof(int*) * rangeSize);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    // temp array of device pointers
+    int** temp_d_ptrs = (int**)malloc(sizeof(int*) * rangeSize);
+    for (int i = 0; i < rangeSize; i++) {
+        cudaStatus = cudaMalloc((void**)&temp_d_ptrs[i], sizeof(int) * len);
+        if (cudaStatus != cudaSuccess) {
+            fprintf(stderr, "cudaMalloc failed!");
+            goto Error;
+        }
+
+        cudaStatus = cudaMemcpy(temp_d_ptrs[i], arr[i], sizeof(int) * len, cudaMemcpyHostToDevice);
+        if (cudaStatus != cudaSuccess) {
+            fprintf(stderr, "cudaMalloc failed!");
+            goto Error;
+        }
+    }
+    cudaStatus = cudaMemcpy(dev_ranges, temp_d_ptrs, sizeof(int*) * rangeSize, cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMemcpy(dev_password, password, len * sizeof(int), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+    
+
+
+    // Launch a kernel on the GPU with one thread for each element.
+    //addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
+
+    // Check for any errors launching the kernel
+    cudaStatus = cudaGetLastError();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+        goto Error;
+    }
+
+    // cudaDeviceSynchronize waits for the kernel to finish, and returns any errors encountered during the launch.
+    cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+        goto Error;
+    }
+
+    // Copy output vector from GPU buffer to host memory.
+    /*cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }*/
+
+Error:
+    for (int i = 0; i < rangeSize; i++) {
+        cudaFree(temp_d_ptrs[i]);
+    }
+    cudaFree(dev_password);
+    cudaFree(dev_ranges);
+    free(temp_d_ptrs);
+
+    return cudaStatus;
+}
+
+
+// Links
+
+
+
+// https://stackoverflow.com/questions/46555270/cuda-copying-an-array-of-arrays-filled-with-data-from-host-to-device
